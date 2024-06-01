875003cb813934aff59dfc6499a133a19066e731
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-06-01 18:15:53 +0200

    Add collapse sidebar logic and style

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './index-style.css';
+import { collapseSidebar, enableSidebar } from './modules/toggleSidebar';
 
+const body = document.querySelector('body');
+const sidebar = document.querySelector('.sidebar');
+
+// event listener for toggling sidebar
+const toggleSidebar = document.getElementById('toggle-sidebar');
+toggleSidebar.addEventListener('click', (e) => {
+    if (sidebar.classList.contains('collapsed')) {
+        enableSidebar();
+    } else {
+        collapseSidebar();
+    }
+});
---
+++ src/modules/toggleSidebar.js
@@ -0,0 +1,19 @@
+import './sidebar-style.css';
+
+const sidebar = document.querySelector('.sidebar');
+const toggleSidebarBtn = document.getElementById('toggle-sidebar');
+
+
+function collapseSidebar() {
+    sidebar.classList.remove('visible');
+    sidebar.classList.add('collapsed');
+    toggleSidebarBtn.classList.add('offset');
+}
+
+function enableSidebar() {
+    sidebar.classList.remove('collapsed');
+    sidebar.classList.add('visible');
+    toggleSidebarBtn.classList.remove('offset');
+}
+
+export { collapseSidebar, enableSidebar };
